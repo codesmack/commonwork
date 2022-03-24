944172052ad00603f84b496d0f44737a0caf71b3
codesmack <92647602+codesmack@users.noreply.github.com>
2022-03-24 23:25:41 +0530

    Create sample.cs

---
+++ sample.cs
@@ -0,0 +1,23 @@
+class TaskThrottle
+    {
+        List<Task> lstTask = new List<Task>();
+
+        public void AddWork(Func<int, int> taskToRun, int j)
+        {
+            if (lstTask.Count > 0 && lstTask.Select(x => x.Status != TaskStatus.RanToCompletion).Count() % 3 == 0)
+            {
+                Console.WriteLine("wait for 4-3 task");
+                Task.WaitAll(lstTask.ToArray());
+            }
+
+            lstTask.Add(Task.Run(() => taskToRun(j)));
+        }
+
+        public void AllWorkOver()
+        {
+            Console.WriteLine("wait for remaining");
+            Task.WaitAll(lstTask.ToArray());
+            Console.WriteLine("all over");
+            lstTask.Clear();
+        }
+    }
